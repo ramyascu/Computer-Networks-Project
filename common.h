3a3c6fcd5f7ab8ef742464330abaf4c2af10acaf
Ramya Padmanabhan <rpadmanabhan@scu.edu>
2018-03-05 21:42:22 -0800

    server.c is compilable
    Also added common.h

---
+++ common.h
@@ -0,0 +1,36 @@
+
+struct __attribute__((__packed__)) dataPacketHdr
+{
+  uint16_t startOfPacket;
+  uint8_t clientId;
+  uint16_t packetType;
+  uint8_t segNum;
+  uint8_t payloadLength;
+};
+
+struct __attribute__((__packed__)) ackPacket
+{
+  uint16_t startOfPacket;
+  uint8_t clientId;
+  uint16_t packetType;
+  uint8_t recvSegNum;
+  uint16_t endOfPacket;
+};
+
+struct __attribute__((__packed__)) rejectPacket
+{
+  uint16_t startOfPacket;
+  uint8_t clientId;
+  uint16_t packetType;
+  uint16_t rejectSubCode;
+  uint8_t recvSegNum;
+  uint16_t endOfPacket;
+}; 
+
+
+enum REJECTION_CODE{
+  REJECTION_CODE__OUT_OF_SEQUENCE=0xFFF4,
+  REJECTION_CODE__LENGTH_MISMATCH,
+  REJECTION_CODE__END_OF_PACKET_MISSING,
+  REJECTION_CODE__DUPLICATE_PACKET,
+};
--- server.c
+++ server.c
@@ -3,36 +3,27 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
-
-struct __attribute__((__packed__)) dataPacketHdr{
-  uint16_t startOfPacket;
-  uint8_t clientId;
-  uint16_t packetType;
-  uint8_t segNum;
-  uint8_t payloadLength;
-};
-
-enum REJECTION_CODE{
-  REJECTION_CODE__OUT_OF_SEQUENCE=0xFFF4,
-  REJECTION_CODE__LENGTH_MISMATCH,
-  REJECTION_CODE__END_OF_PACKET_MISSING,
-  REJECTION_CODE__DUPLICATE_PACKET,
-};
+#include "common.h"
 
 int udpSocket;
-char inBuffer[1024];
-char outBuffer[16];
-char payloadBuffer[256];
+uint8_t inBuffer[1024];
+uint8_t payloadBuffer[256];
 
 socklen_t addr_size;
 int i;
 
 
-int main(){
+int recvDataPacket(struct dataPacketHdr *hdr, uint8_t *payload, uint16_t *endOfPacket, struct sockaddr_in *clientAddr);
+void sendRejectPacket(uint8_t clientId, uint16_t rejectSubCode, uint8_t recvSegNum, struct sockaddr_in *clientAddr);
+void sendAckPacket(uint8_t clientId, uint8_t recvSegNum, struct sockaddr_in *clientAddr);
 
+
+int main()
+{
   int nBytes;
   struct sockaddr_in serverAddr;
   uint8_t clientId;
@@ -58,37 +49,53 @@ int main(){
     uint16_t recvStatus;
 
 
-    recvStatus = recvPacket(&hdr, payloadBuffer, &endOfPacket, &clientAddr);
+    recvStatus = recvDataPacket(&hdr, payloadBuffer, &endOfPacket, &clientAddr);
 
     if(recvStatus == 0)
     {
       // Check if segment number is expected
-      if(hdr->segNum != nextSegment)
+      if(hdr.segNum != nextSegment)
       {
-        printf("ERROR: Server expected %d segment, but received %d segment\n", nextSegment, hdr->segNum);
+        if(hdr.segNum != nextSegment - 1)
+        {
+          printf("ERROR: Server expected %d segment, but received %d segment\n", nextSegment, hdr.segNum);
+          
+          // TODO: Transmit reject packet (out of sequence error)
 
-        // Transmit error 
+          continue;
+        }
+        else
+        {
+          printf("ERROR: Server got a duplicate packet (segment %d)\n", hdr.segNum);
+
+          // TODO: Transmit reject packet (duplicate packet)
+
+          continue;
+        }
       }
+
+
       if(nextSegment == 0)
       {
-        if(hdr->segNum == 0)
+        clientId = hdr.clientId;
       }
 
-      // Send acknowledgement packet to client.
+      // Send acknowledgement packet to client
     }
-
-    /*Send uppercase message back to client, using serverStorage as the address*/
-    sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
+    else
+    {
+      // TODO: Send appropriate reject packet for packet error
+    }
   }
 
-
-  return 0;
+return 0;
 }
 
 
-int recvPacket(struct dataPacketHdr *hdr, uint8_t *payload, uint16_t *endOfPacket, struct sockaddr_in *clientAddr)
+int recvDataPacket(struct dataPacketHdr *hdr, uint8_t *payload, uint16_t *endOfPacket, struct sockaddr_in *clientAddr)
 {
   uint16_t *endOfPacket1;
+  uint32_t nBytes;
   socklen_t addrSize = sizeof(struct sockaddr_in);
 
   // Receive a packet from the client
@@ -99,19 +106,45 @@ int recvPacket(struct dataPacketHdr *hdr, uint8_t *payload, uint16_t *endOfPacke
 
   // Check if the length field in the packet is consistent with the packet size
   if(hdr->payloadLength != nBytes - sizeof(struct dataPacketHdr) - 2)
-    return 1;
+    return REJECTION_CODE__LENGTH_MISMATCH;
 
   // Decode the end-of-packet marker
-  endofPacket1 = inBuffer + sizeof(struct dataPacketHdr) + hdr->payloadLength;
+  endOfPacket1 = (uint16_t *) (inBuffer + sizeof(struct dataPacketHdr) + hdr->payloadLength);
   *endOfPacket = *endOfPacket1;
 
   // Validate end-of-packet marker
   if(*endOfPacket != 0xFFFF)
-    return 2;
+    return REJECTION_CODE__END_OF_PACKET_MISSING;
 
   // Copy payload in buffer to the payload destination address
   memcpy(payload, inBuffer + sizeof(struct dataPacketHdr), hdr->payloadLength);
 
   // Packet has no structural errors
   return 0;
+}
+
+void sendAckPacket(uint8_t clientId, uint8_t recvSegNum, struct sockaddr_in *clientAddr)
+{
+  struct ackPacket ack;
+  ack.startOfPacket = 0xFFFF;
+  ack.clientId = clientId;
+  ack.packetType = 0xFFF2;
+  ack.recvSegNum = recvSegNum;
+  ack.endOfPacket = 0xFFFF;
+
+  sendto(udpSocket, &ack, sizeof(ack), 0, (struct sockaddr *) clientAddr, sizeof(struct sockaddr_in));
+}
+
+void sendRejectPacket(uint8_t clientId, uint16_t rejectSubCode, uint8_t recvSegNum, struct sockaddr_in *clientAddr)
+{
+  struct rejectPacket rej;
+
+  rej.startOfPacket = 0xFFFF;
+  rej.clientId = clientId;
+  rej.packetType = 0xFFF3;
+  rej.rejectSubCode = rejectSubCode;
+  rej.recvSegNum = recvSegNum;
+  rej.endOfPacket = 0xFFFF;
+
+  sendto(udpSocket, &rej, sizeof(rej), 0, (struct sockaddr *) clientAddr, sizeof(struct sockaddr_in));
 }
